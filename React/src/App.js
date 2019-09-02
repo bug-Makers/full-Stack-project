73d6ab9715f9e4731c264917d09008760998c123
Hala Almomani <you@example.com>
2019-09-02 15:34:05 +0300

    structure of server and database

--- React/src/App.js
+++ React/src/App.js
@@ -1,21 +1,22 @@
-import React ,{Component}from 'react';
-const axios = require('axios');
-class App extends Component{
-  state={ }
+import React, { Component } from 'react';
+
+// const axios = require('axios')
 
 
-  render(){
-   
-  return (
-  <>
- 
-<h1>bug Makers group</h1>
-    </>
-  );
-}
+class App extends Component {
+
+
+  render() {
+
+    return (
+
+<h1>bug Makers </h1>
+
+     
+    );
+  }
 }
 export default App;
 
 
 
-  
