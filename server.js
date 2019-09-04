3c4255ffed8eee403c16f3ebac503efbb5767027
HalaAlmomanii <51744154+HalaAlmomanii@users.noreply.github.com>
2019-09-04 12:02:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ app.use(cors());
 app.use("/User",USER);
 app.use("/Doctor",DOCTOR);
 
+Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 const PORT =  process.env.PORT|| 9000;
+
+
+
 app.listen(PORT, () => console.log(`Server listening to ${PORT}`));
 
 
